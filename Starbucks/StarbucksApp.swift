40db497382b87e7482cd5a7a1c4c197cf84024ae
mino <kmh922@naver.com>
2023-11-15 15:15:24 +0900

    Initial Commit

---
+++ Starbucks/StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by DEV IOS on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
