29cea8e858cd6cc8e03bac18fd89817475469249
mino <kmh922@naver.com>
2023-11-22 17:38:31 +0900

    Chore: Home Logic 수정

--- Starbucks/Presentation/Home/Home.swift
+++ Starbucks/Presentation/Home/Home.swift
@@ -5,144 +5,201 @@
 //  Created by mino on 2023/10/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 struct Home: View {
-    @StateObject var homeData = HomeViewModel()
     @Environment(\.colorScheme) var scheme
-    @State var scrollDown: Bool = true
-    @State var checkOffsetValue: CGFloat = 0.0
-    @State var progressbarPercent: Double = 0
-    @State var deleveryButtonToggle: Bool = false
-    @State private var recommedCoffee: [RecommendCoffee] = [RecommendCoffee(title: "아이스 아메리카노", coffeeImage: "food"), RecommendCoffee(title: "아이스 뭐라카노", coffeeImage: "food"), RecommendCoffee(title: "아이스 디카페인 카페 아메리카노", coffeeImage: "food")]
-    @State private var foodList: [String] = ["choclate", "cookie", "food", "fries", "pizza", "sandwich"]
-    @State private var noticeList: [HomeNotice] = HomeNotice.mock
+    private let store: StoreOf<HomeReducer>
+    @ObservedObject var viewStore: ViewStoreOf<HomeReducer>
+    
+    init(store: StoreOf<HomeReducer>) {
+        self.store = store
+        viewStore = ViewStore(self.store, observe: { $0 })
+    }
+}
+//MARK: - View
+extension Home {
     var body: some View {
         NavigationStack {
             ZStack {
                 ScrollView {
-                    GeometryReader { reader -> AnyView in
-                        let offset = reader.frame(in: .global).minY
-                        let checkOffsetValue = offset
-                        if -offset > 0 {
-                            DispatchQueue.main.async {
-                                self.homeData.offset = -offset
-                            }
-                        }
-                        return AnyView(Text("").frame(width: 0, height: 0))
-                    }
-                    VStack {
-                        Image("food")
-                            .resizable()
-                            .ignoresSafeArea()
-//                                        .frame(height: 150 + (offset > 0 ? offset : 0))
-                        VStack(alignment: .leading) {
-                            Text("15 ★ until Gold Level")
-                                .foregroundColor(.green)
-                            HStack {
-                                ProgressView("", value: progressbarPercent, total: 100)
-                                    .padding(.trailing, 5)
-                                    .tint(.green)
-                                    .animation(.easeInOut(duration: 2), value: progressbarPercent)
-                                Text("10")
-                                    .font(.largeTitle)
-                                    .fontWeight(.semibold)
-                                Text("/")
-                                    .foregroundColor(.gray)
-                                Text("25★")
-                                    .font(.title3)
-                                    .foregroundColor(.green)
-                                    .padding(.trailing, 25)
-                            }
-                            .onAppear {
-                                self.progressbarPercent = 60
-                            }
-                        }
-                        .padding(.leading, 25)
-                        .padding(.top, 10)
-                    }
-                    .frame(height: 220 + (checkOffsetValue > 0 ? checkOffsetValue : 0))
-                    .background(.white)
-                    .opacity(homeData.offset > 150 ? 0 : 1)
-                    .animation(.easeOut, value: homeData.offset)
-
-                    LazyVStack(alignment: .center, spacing: 0, pinnedViews: [.sectionHeaders], content: {
-                            Section(header: HomeHeader()) {
-                                eventCard
-                                noticeHeader
-                                noticeCard
-                                
-                                configureDefaultHeader("고객님을 위한 추천 메뉴")
-                                recommendCard
-                                
-                                configureDefaultHeader("하루가 달콤해지는 디저트")
-                                recommendCard
-                                
-                                eventCard
-                            }
-                    })
-                    .onChange(of: homeData.offset) { [offset = homeData.offset] newValue in
-            //            print("homeData offset:", homeData.offset)
-                        print("이동 후 값:", newValue)
-                        print("변하기 전 값:", offset)
-                        
-                        
-                        if offset - CGFloat(4000.0) < newValue - CGFloat(4000.0) {
-            //                print("offset이 직전보다 작음 -> 스크롤이 내려가고 있음")
-                            scrollDown = true
-                        } else {
-            //                print("offset이 직전보다 큼 -> 스크롤이 올라가고 있음")
-                            scrollDown = false
-                        }
-                    }
                     
+                    LazyVStack(alignment: .center,
+                               pinnedViews: .sectionHeaders){
+                        
+                        Section {
+                            homeGeometryHelper
+                            homeTopSectionView
+                        }
+                        
+                        Section(header: homeStickyHeader) {
+                            homeEventListView
+                        }
+                        
+                        Section {
+                            configureDefaultSectionHeader("고객님을 위한 추천 메뉴")
+                            recommendMenuItem
+                        }
+                        
+                        Section {
+                            configureDefaultSectionHeader("하루가 달콤해지는 디저트")
+                            recommendMenuItem
+                        }
+                        
+                        Section {
+                            noticeSectionHeader
+                            noticeSectionItem
+                        }
+                        
+                        Section {
+                            homeEventListView
+                        }
+                        
+                    }
                 }
                 .overlay(
-                    // Only Safe Area
                     (scheme == .dark ? Color.black : Color.white)
                         .frame(height: UIApplication.shared.windows.first?.safeAreaInsets.top)
                         .ignoresSafeArea(.all, edges: .top)
-                        .opacity(homeData.offset > 190 ? 1 : 0)
+                        .opacity(viewStore.offset > 190 ? 1 : 0)
                     ,alignment: .top
                 )
-                // Used it EnvironmentObject for accessing all sub objects
-                .environmentObject(homeData)
-                
-                VStack {
-                    Spacer()
-                    HStack {
-                        //
-                        Button {
-                            deleveryButtonToggle.toggle()
-                        } label: {
-                            ZStack {
-                                Circle().fill(.white)
-                                Text("배달버튼 체크")
-                                    .lineLimit(2)
-                            }.frame(width: 100, height: 100)
-                        }
-                        .hidden()
-                        //
-                        Spacer()
-                        Capsule()
-                            .fill(.green)
-                            .frame(width: scrollDown ? 70 : 210, height: 70)
-                            .animation(.easeIn, value: scrollDown)
-                            .padding(.trailing, 10)
-                    }
-                }
-                .animation(.easeOut, value: deleveryButtonToggle)
+                deleveryButton
             }
         }
     }
 }
-
+//MARK: - Preview
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home(store: Store(initialState: .init(), reducer: { HomeReducer() }))
+    }
+}
+//MARK: -
 extension Home {
-    private var eventCard: some View {
+    /// 스크롤에 따른 offset 위치를 체크하기 위한 geometryReader 입니다.
+    private var homeGeometryHelper: some View {
+        GeometryReader { reader -> AnyView in
+            let offset = reader.frame(in: .global).minY
+            if -offset > 0 {
+                DispatchQueue.main.async {
+                    //                    self.viewModel.offset = -offset
+                    viewStore.send(.getOffset(-offset))
+                }
+            }
+            return AnyView(Text("").frame(width: 0, height: 0))
+        }
+    }
+    /// 최상단 이미지와 progressbar stack까지 포함하는 뷰입니다.
+    private var homeTopSectionView: some View {
+        VStack {
+            Image("food")
+                .resizable()
+
+            VStack(alignment: .leading) {
+                Text("15 ★ until Gold Level")
+                    .foregroundColor(.green)
+                HStack {
+                    ProgressView("", value: viewStore.progressbarPercent, total: 100)
+                        .padding(.trailing, 5)
+                        .tint(.green)
+                        .animation(.easeInOut(duration: 2), value: viewStore.progressbarPercent)
+                    Text("10")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    Text("/")
+                        .foregroundColor(.gray)
+                    Text("25★")
+                        .font(.title3)
+                        .foregroundColor(.green)
+                        .padding(.trailing, 25)
+                }
+                .onAppear { viewStore.send(.onAppear) }
+            }
+            .padding(.leading, 25)
+            .padding(.top, 10)
+        }
+        .frame(height: 220)
+        .background(.white)
+        .opacity(viewStore.offset > 150 ? 0 : 1)
+        .animation(.easeOut, value: viewStore.offset)
+    }
+    /// 알림정보, 쿠폰 등 section의 Header로 사용되는 뷰 입니다.
+    private var homeStickyHeader: some View {
+        VStack {
+            HStack {
+                Button {
+                    viewStore.send(.whatsNewButtonTapped)
+                } label: {
+                    whatsNewLabel
+                }
+                .padding(.trailing, 20)
+                
+                Button {
+                    viewStore.send(.couponButtonTapped)
+                } label: {
+                    couponLabel
+                }
+                
+                Spacer()
+                Button {
+                    viewStore.send(.noticeButtonTapped)
+                } label: {
+                    noticeLabel
+                }
+            }
+            .navigationDestination(store: self.store.scope(state: \.$destination, action: {  .destination($0)}), state: /HomeReducer.Destination.State.notice, action: HomeReducer.Destination.Action.notice, destination: { store in
+                NoticeView(store: store)
+            })
+            .navigationDestination(store: self.store.scope(state: \.$destination, action: {  .destination($0)}), state: /HomeReducer.Destination.State.coupon, action: HomeReducer.Destination.Action.coupon, destination: { store in
+                CouponView(store: store)
+            })
+            .navigationDestination(store: self.store.scope(state: \.$destination, action: {  .destination($0)}), state: /HomeReducer.Destination.State.whatsNew, action: HomeReducer.Destination.Action.whatsNew, destination: { store in
+                WhatsNewView(store: store)
+            })
+            .frame(height: 40)
+            .padding([.leading, .trailing], 20)
+            Divider()
+        }
+        .background(.white)
+    }
+    /// [WhatsNew] 'homeStickyHeader'의 내부 버튼의 label을 위해서 사용되는 뷰입니다.
+    private var whatsNewLabel: some View {
+        HStack {
+            Image(systemName: "envelope")
+                .resizable()
+                .frame(width: 30, height: 20)
+                .foregroundColor(.black).opacity(0.5)
+            Text("What's New")
+                .foregroundColor(.black)
+                .font(.subheadline)
+        }
+    }
+    /// [Coupon] 'homeStickyHeader'의 내부 버튼의 label을 위해서 사용되는 뷰입니다.
+    private var couponLabel: some View {
+        HStack {
+            Image(systemName: "creditcard")
+                .resizable()
+                .frame(width: 30, height: 20)
+                .foregroundColor(.black).opacity(0.5)
+            Text("Coupon")
+                .foregroundColor(.black)
+                .font(.subheadline)
+        }
+    }
+    /// [Notice] 'homeStickyHeader'의 내부 버튼의 label을 위해서 사용되는 뷰입니다.
+    private var noticeLabel: some View {
+        Image(systemName: "bell")
+            .resizable()
+            .frame(width: 20, height: 20)
+            .foregroundColor(.black).opacity(0.5)
+    }
+    /// 사진 공지사항 리스트를 보여주는 뷰 입니다.
+    private var homeEventListView: some View {
         VStack(alignment: .center, spacing: 10, content: {
             
-            ForEach(foodList, id: \.self) { foodName in
-                
+            ForEach(viewStore.foodList, id: \.self) { foodName in
                 Image(foodName)
                     .resizable()
                     .frame(height: foodName == "sandwich" ? 300 : 180)
@@ -152,8 +209,8 @@ extension Home {
         .padding([.leading, .trailing], 10)
         .padding(.bottom, 40)
     }
-    
-    private var noticeHeader: some View {
+    /// Label과 button이 있는 Section Header 입니다.
+    private var noticeSectionHeader: some View {
         HStack {
             Text("What's New")
                 .font(.title2)
@@ -165,11 +222,11 @@ extension Home {
             .foregroundColor(.green)
         }.padding([.leading, .trailing], 20)
     }
-    
-    private var noticeCard: some View {
+    /// 수평스크롤이 가능한 noticeSection 전용 뷰 입니다.
+    private var noticeSectionItem: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
-                ForEach(noticeList, id: \.self) { list in
+                ForEach(viewStore.noticeList, id: \.self) { list in
                     VStack(alignment: .leading) {
                         Image(list.thumbnail)
                             .resizable()
@@ -196,11 +253,11 @@ extension Home {
         .padding(.bottom, 40)
         .frame(height: 250)
     }
-    
-    private var recommendCard: some View {
+    /// 수평스크롤이 가능한 음료, 디저트 추천 뷰 입니다.
+    private var recommendMenuItem: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
-                ForEach(recommedCoffee, id: \.self) { list in
+                ForEach(viewStore.recommendCoffee) { list in
                     VStack(alignment: .center) {
                         Image(list.coffeeImage)
                             .resizable()
@@ -220,8 +277,8 @@ extension Home {
         .padding(.bottom, 40)
         .frame(height: 250)
     }
-    
-    private func configureDefaultHeader(_ title: String) -> some View {
+    /// Label만 존재하는 Section Title입니다.
+    private func configureDefaultSectionHeader(_ title: String) -> some View {
         return HStack {
             Text(title)
                 .font(.title2)
@@ -229,10 +286,38 @@ extension Home {
             Spacer()
         }.padding(.leading, 20)
     }
-}
+    /// 우측 하단의 배달 버튼
+    private var deleveryButton: some View {
+        VStack {
+            Spacer()
+            HStack(alignment: .center) {
+                Spacer()
+                ZStack(alignment: .leading) {
+                    Capsule()
+                        .fill(.green)
+                        .frame(width: viewStore.deleveryButtonSize, height: 70)
+                        .padding(.trailing, 10)
+                        .overlay {
+                            Image(systemName: "heart")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .padding(.trailing, viewStore.scrollUp ? 10 : 140)
+//                                .padding(25)
+                            Text("Delevery")
+                                .font(.title)
+                                .opacity(viewStore.scrollUp ? 0 : 1)
+                                .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 0))
+                        }
+                        .foregroundColor(.white)
 
-struct Home_Previews: PreviewProvider {
-    static var previews: some View {
-        Home()
+//                    Image(systemName: "heart")
+//                        .padding(25)
+//                    Text("text")
+//                        .padding(.leading, 90)
+                    
+                }
+                .animation(.easeIn, value: viewStore.deleveryButtonSize)
+            }
+        }
     }
 }
