ac64683bfe250480bc0a8422df25405347ac6318
mino <kmh922@naver.com>
2023-11-23 09:58:19 +0900

    feat: add AddCouponReducer

---
+++ Starbucks/Presentation/Home/Coupon/AddCoupon/AddCouponReducer.swift
@@ -0,0 +1,25 @@
+//
+//  AddCouponReducer.swift
+//  Starbucks
+//
+//  Created by DEV IOS on 2023/11/22.
+//
+
+import ComposableArchitecture
+import Foundation
+
+struct AddCouponReducer: Reducer {
+//    struct Destination: Reducer {
+//        struct State: Equatable {}
+//        enum Action: Equatable {}
+//        var body: some ReducerOf<Self> {}
+//    }
+    
+    struct State: Equatable {}
+    enum Action: Equatable {}
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {}
+        }
+    }
+}
