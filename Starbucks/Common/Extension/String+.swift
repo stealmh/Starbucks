6beb49497169ca3fce7ceb2d7e73c1b98347b4d1
mino <kmh922@naver.com>
2023-12-08 15:57:38 +0900

    feat: String Extension 추가 (카드번호 마스킹 함수)

--- Starbucks/Common/Extension/String+.swift
+++ Starbucks/Common/Extension/String+.swift
@@ -9,4 +9,25 @@ import Foundation
 
 extension String {
     static let imageNameMock = ["choclate", "cookie", "food", "fries", "pizza", "sandwich"]
+    /// 카드 비밀번호를 뒤 `6`자리를 제외하고 가립니다.
+    var hideCardNumber: String {
+        var result = ""
+        
+        // Remove non-numeric characters
+        let numericString = self.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        
+        for (idx, number) in numericString.enumerated() {
+            
+            if idx >= 10 {
+                result += String(number)
+            } else {
+                result += "*"
+            }
+            
+            if idx == 3 || idx == 7 || idx == 11 {
+                result += "-"
+            }
+        }
+        return result
+    }
 }
